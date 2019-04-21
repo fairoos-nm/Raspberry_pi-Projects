2a63dc8a73e38ff81c7d11dcf9bc7ff7961f6ebf
Rpi_fairoos <fairoosnm123@gmail.com>
2019-04-21 14:51:58 +0000

    Adds program to measure distance using ultrasonic sensor

---
+++ distence.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setwarnings(False)
+
+GPIO.setmode(GPIO.BCM)
+
+TRIG = 4
+ECHO = 18
+
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+
+GPIO.output(TRIG, True)
+time.sleep(0.00001)
+GPIO.output(TRIG, False)
+
+while GPIO.input(ECHO) == False:
+    s = time.time()
+while GPIO.input(ECHO) == True:
+    end = time.time()
+
+sig_time = end - s
+
+
+#CM:
+distance = sig_time / 0.000058
+
+#inches:
+#distance = sig_time / 0.000148
+
+print('Distance: {} centimeters'.format(distance))
+
+GPIO.cleanup()
